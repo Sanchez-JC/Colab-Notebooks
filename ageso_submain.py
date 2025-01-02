b54d342f30a100905e8665ed801369fffa16e7c5
Juan C. Sánchez M. <jcsanchez@correo.uis.edu.co>
2025-01-02 10:45:30 -0500

    Create ageso_submain.py

---
+++ ageso_submain.py
@@ -0,0 +1,17 @@
+def submain(numero_de_archivos):
+  archivos = os.listdir()
+  archivos.remove("Plantilla_0.xlsx")
+  archivos.remove(".config")
+  archivos.remove("sample_data")
+  try:
+    archivos.remove(".ipynb_checkpoints")
+  except:
+    pass
+
+  #IMPORTA Y ABRE ARCHIVO
+  ruta_datos = archivos[numero_de_archivos]
+  datos = pd.read_excel(ruta_datos)
+  ruta_plantilla = "/content/Plantilla_0.xlsx"
+  plantilla = openpyxl.load_workbook(ruta_plantilla)
+  total_trabajadores = datos.shape[numero_de_archivos]
+
