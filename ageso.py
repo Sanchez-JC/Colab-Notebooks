535ff4b6bea313558d1aa59b4957af94f9270f6c
Juan C. Sánchez M. <jcsanchez@correo.uis.edu.co>
2025-01-04 11:08:55 -0500

    Update ageso.py

--- ageso.py
+++ ageso.py
@@ -68,9 +68,7 @@ def popup(mensaje):
 ### FUNCIÓN PRINCIPAL ###
 archivos = listar_directorio()
 def main(indice_archivos):
-  print(archivos)
-  print(indice_archivos)
-  print(archivos[indice_archivos])
+
  #IMPORTA Y ABRE ARCHIVO
   ruta_datos = archivos[indice_archivos]
   datos = pd.read_excel(ruta_datos)
