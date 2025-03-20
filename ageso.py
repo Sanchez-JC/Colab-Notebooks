a528f2daffb4238d857c4670f0a7695fe498ce0f
Juan C. Sánchez M. <jcsanchez@correo.uis.edu.co>
2025-03-19 20:28:32 -0500

    Update ageso.py

--- ageso.py
+++ ageso.py
@@ -562,10 +562,9 @@ def main(indice_archivos):
 
   #Clasificador y contador
   etiquetas_imc = ["Bajo Peso", "Normal", "Sobrepeso", "Obesidad I o Leve", "Obesidad II o Moderada", "Obesidad III o Severa", "Sin información"]
-  elementos_imc = [sum(1 for i in imc if i <= 18), sum(1 for i in imc if 18 <= i < 25),
+  elementos_imc = [sum(1 for i in imc if 1 <= i <= 18), sum(1 for i in imc if 18 <= i < 25),
                    sum(1 for i in imc if 25 <= i < 30), sum(1 for i in imc if 30 <= i < 35),
-                   sum(1 for i in imc if 35 <= i < 40), sum(1 for i in imc if i >= 40)]
-  elementos_imc.append(abs(total_trabajadores - len(elementos_imc)))
+                   sum(1 for i in imc if 35 <= i < 40), sum(1 for i in imc if i >= 40), sum(1 for i in imc if i == 0)]
 
   #Validación
   if sum(elementos_imc) != total_trabajadores:
